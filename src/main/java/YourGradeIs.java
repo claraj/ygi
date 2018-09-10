33bbd0b41ed9c356104ff805cbb4af1e1d282762
clara <clara@minneapolis.edu>
2018-09-10 12:47:25 -0500

    hello world plugin

---
+++ src/main/java/YourGradeIs.java
@@ -0,0 +1,13 @@
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "ygi")
+public class YourGradeIs extends AbstractMojo {
+
+    public void execute() throws MojoExecutionException {
+        getLog().info("hello world");
+    }
+
+
+}
